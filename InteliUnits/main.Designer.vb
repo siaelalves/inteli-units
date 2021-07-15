82c6fff86ad524cc7e1aed441318d15100fdad89
Siael Alves <32615727+siaelalves@users.noreply.github.com>
2021-07-15 12:43:02 -0300

    Upload inicial

---
+++ InteliUnits/main.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTt = New System.Windows.Forms.Label()
+        Me.txtEnt = New System.Windows.Forms.ComboBox()
+        Me.txtSd = New System.Windows.Forms.ComboBox()
+        Me.lblEnt = New System.Windows.Forms.Label()
+        Me.lblSd = New System.Windows.Forms.Label()
+        Me.txtVlSd = New System.Windows.Forms.TextBox()
+        Me.txtVlEnt = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblTt
+        '
+        Me.lblTt.AutoSize = True
+        Me.lblTt.Location = New System.Drawing.Point(12, 9)
+        Me.lblTt.Name = "lblTt"
+        Me.lblTt.Size = New System.Drawing.Size(119, 13)
+        Me.lblTt.TabIndex = 0
+        Me.lblTt.Text = "Conversor de unidades:"
+        '
+        'txtEnt
+        '
+        Me.txtEnt.FormattingEnabled = True
+        Me.txtEnt.Items.AddRange(New Object() {"B", "KB", "MB", "GB"})
+        Me.txtEnt.Location = New System.Drawing.Point(157, 51)
+        Me.txtEnt.Name = "txtEnt"
+        Me.txtEnt.Size = New System.Drawing.Size(77, 21)
+        Me.txtEnt.TabIndex = 1
+        Me.txtEnt.Text = "B"
+        '
+        'txtSd
+        '
+        Me.txtSd.FormattingEnabled = True
+        Me.txtSd.Items.AddRange(New Object() {"B", "KB", "MB", "GB"})
+        Me.txtSd.Location = New System.Drawing.Point(382, 51)
+        Me.txtSd.Name = "txtSd"
+        Me.txtSd.Size = New System.Drawing.Size(77, 21)
+        Me.txtSd.TabIndex = 2
+        Me.txtSd.Text = "B"
+        '
+        'lblEnt
+        '
+        Me.lblEnt.AutoSize = True
+        Me.lblEnt.Location = New System.Drawing.Point(12, 33)
+        Me.lblEnt.Name = "lblEnt"
+        Me.lblEnt.Size = New System.Drawing.Size(47, 13)
+        Me.lblEnt.TabIndex = 5
+        Me.lblEnt.Text = "Entrada:"
+        '
+        'lblSd
+        '
+        Me.lblSd.AutoSize = True
+        Me.lblSd.Location = New System.Drawing.Point(237, 33)
+        Me.lblSd.Name = "lblSd"
+        Me.lblSd.Size = New System.Drawing.Size(39, 13)
+        Me.lblSd.TabIndex = 6
+        Me.lblSd.Text = "Saída:"
+        '
+        'txtVlSd
+        '
+        Me.txtVlSd.Location = New System.Drawing.Point(240, 52)
+        Me.txtVlSd.Name = "txtVlSd"
+        Me.txtVlSd.ReadOnly = True
+        Me.txtVlSd.Size = New System.Drawing.Size(136, 20)
+        Me.txtVlSd.TabIndex = 7
+        Me.txtVlSd.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtVlEnt
+        '
+        Me.txtVlEnt.Location = New System.Drawing.Point(15, 51)
+        Me.txtVlEnt.Name = "txtVlEnt"
+        Me.txtVlEnt.Size = New System.Drawing.Size(136, 20)
+        Me.txtVlEnt.TabIndex = 8
+        Me.txtVlEnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 87)
+        Me.Controls.Add(Me.txtVlEnt)
+        Me.Controls.Add(Me.txtVlSd)
+        Me.Controls.Add(Me.lblSd)
+        Me.Controls.Add(Me.lblEnt)
+        Me.Controls.Add(Me.txtSd)
+        Me.Controls.Add(Me.txtEnt)
+        Me.Controls.Add(Me.lblTt)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "main"
+        Me.Text = "I N T E L I   U N I T S"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTt As System.Windows.Forms.Label
+    Friend WithEvents txtEnt As System.Windows.Forms.ComboBox
+    Friend WithEvents txtSd As System.Windows.Forms.ComboBox
+    Friend WithEvents lblEnt As System.Windows.Forms.Label
+    Friend WithEvents lblSd As System.Windows.Forms.Label
+    Friend WithEvents txtVlSd As System.Windows.Forms.TextBox
+    Friend WithEvents txtVlEnt As System.Windows.Forms.TextBox
+
+End Class
---
+++ InteliUnits/main.vb
@@ -0,0 +1,127 @@
+﻿Public Class main
+
+    Function Convert(ByVal valorDeEntrada As Double, ByVal unidadeDeEntrada As String, _
+                     ByVal unidadeDeSaida As String) As Double
+
+        If unidadeDeEntrada = unidadeDeSaida Then
+            Return txtVlEnt.Text
+
+        ElseIf unidadeDeEntrada = "b" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text / 8
+
+
+
+        ElseIf unidadeDeEntrada = "KB" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * 1024
+
+        ElseIf unidadeDeEntrada = "Kb" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * 128
+
+
+
+        ElseIf unidadeDeEntrada = "MB" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * (1024 * 1024)
+
+        ElseIf unidadeDeEntrada = "Mb" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * 128 * 1024
+
+
+
+        ElseIf unidadeDeEntrada = "GB" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * (1024 * 1024 * 1024)
+
+        ElseIf unidadeDeEntrada = "Gb" And unidadeDeSaida = "B" Then
+            Return txtVlEnt.Text * 128 * (1024 * 1024)
+
+
+
+        End If
+        '
+        If unidadeDeEntrada = "B" And unidadeDeSaida = "KB" Then
+            Return txtVlEnt.Text / 1024
+
+        ElseIf unidadeDeEntrada = "b" And unidadeDeSaida = "KB" Then
+            Return txtVlEnt.Text * (txtEnt.Text / 8) * 1024
+
+
+
+        ElseIf unidadeDeEntrada = unidadeDeSaida Then
+            Return txtVlEnt.Text
+
+
+
+        ElseIf unidadeDeEntrada = "Mb" And unidadeDeSaida = "KB" Then
+            Return txtVlEnt.Text * 128
+
+        ElseIf unidadeDeEntrada = "MB" And unidadeDeSaida = "KB" Then
+            Return txtVlEnt.Text * 1024
+
+
+
+        ElseIf unidadeDeEntrada = "Gb" And unidadeDeSaida = "KB" Then
+            Return txtEnt.Text * 128 * 1024
+
+        ElseIf unidadeDeEntrada = "GB" And unidadeDeSaida = "KB" Then
+            Return txtVlEnt.Text * (1024 * 1024)
+
+
+
+        End If
+        '
+        If unidadeDeEntrada = "B" And unidadeDeSaida = "MB" Then
+            Return txtVlEnt.Text / (1024 * 1024)
+
+        ElseIf unidadeDeEntrada = "KB" And unidadeDeSaida = "MB" Then
+            Return txtVlEnt.Text / 1024
+
+        ElseIf unidadeDeEntrada = unidadeDeSaida Then
+            Return txtVlEnt.Text
+
+        ElseIf unidadeDeEntrada = "GB" And unidadeDeSaida = "MB" Then
+            Return txtVlEnt.Text * 1024
+
+        End If
+        '
+        If unidadeDeEntrada = "B" And unidadeDeSaida = "GB" Then
+            Return txtVlEnt.Text / (1024 * 1024 * 1024)
+
+        ElseIf unidadeDeEntrada = "KB" And unidadeDeSaida = "GB" Then
+            Return txtVlEnt.Text / (1024 * 1024)
+
+        ElseIf unidadeDeEntrada = "MB" And unidadeDeSaida = "GB" Then
+            Return txtVlEnt.Text / 1024
+
+        ElseIf unidadeDeEntrada = unidadeDeSaida = "GB" Then
+            Return txtVlEnt.Text
+
+        End If
+
+    End Function
+
+    Private Sub txtVlEnt_TextChanged(sender As Object, e As EventArgs) Handles txtVlEnt.TextChanged
+        Try
+            If txtVlEnt.Text = "" Then txtVlEnt.Text = 0
+            txtVlSd.Text = Convert(txtVlEnt.Text, txtEnt.Text, txtSd.Text)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub txtEnt_SelectedIndexChanged(sender As Object, e As EventArgs) Handles txtEnt.SelectedIndexChanged
+        Try
+            If txtVlEnt.Text = "" Then txtVlEnt.Text = 0
+            txtVlSd.Text = Convert(txtVlEnt.Text, txtEnt.Text, txtSd.Text)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub txtSd_SelectedIndexChanged(sender As Object, e As EventArgs) Handles txtSd.SelectedIndexChanged
+        Try
+            If txtVlEnt.Text = "" Then txtVlEnt.Text = 0
+            txtVlSd.Text = Convert(txtVlEnt.Text, txtEnt.Text, txtSd.Text)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
